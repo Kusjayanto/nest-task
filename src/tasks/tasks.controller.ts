780412a1f14065c8d811f861aabad87901a6a410
Kusjayanto <saputrayogi493@gmail.com>
2023-11-16 22:58:42 +0700

    new project

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dtos/create-task.dto';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+    taskService: TasksService
+
+    constructor() {
+        this.taskService = new TasksService()
+    }
+
+    @Get()
+    listTasks() {
+        return this.taskService.findAll()
+    }
+
+    @Post()
+    createTasks(@Body() body: CreateTaskDto) {
+        return this.taskService.create(body.content)
+    }
+
+    @Get('/:id')
+    async getTasks(@Param('id') id: string) {
+        const task = await this.taskService.findOne(parseInt(id))
+
+        if (!task) {
+            throw new NotFoundException('Task not found')
+        }
+        return task
+    }
+}
